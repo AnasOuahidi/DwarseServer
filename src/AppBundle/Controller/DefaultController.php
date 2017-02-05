4fb9a43b79def5069858703f46827bbd600b28d5
akagami <anasouahidi@gmail.com>
2017-02-05 11:12:45 +0100

    [aouahidi]: Add .htaccess file config

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,13 +4,12 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request) {
+    public function indexAction() {
         return $this->render('default/index.html.twig');
     }
 }
