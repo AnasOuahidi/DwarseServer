1ade157134d2c6ec6d072c54ac57448e6366fb99
akagami <anasouahidi@gmail.com>
2017-02-15 11:34:47 +0100

    [aouahidi]: Configuration des environnements de prod et test

--- app/config/parameters_prod.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
-$container->setParameter('database_driver', 'pdo_mysql');
-$container->setParameter('database_host', $db['host']);
-$container->setParameter('database_port', $db['port']);
-$container->setParameter('database_name', substr($db["path"], 1));
-$container->setParameter('database_user', $db['user']);
-$container->setParameter('database_password', $db['pass']);
