f1a022089d9cc65e54d14d3c5d393181d9ebfb29
akagami <anasouahidi@gmail.com>
2017-02-16 17:13:55 +0100

    [aouahidi]: Création de la page de confirmation de compte

--- src/AuthBundle/Controller/AuthTokenController.php
+++ src/AuthBundle/Controller/AuthTokenController.php
@@ -2,10 +2,12 @@
 
 namespace AuthBundle\Controller;
 
+use AuthBundle\Entity\AuthToken;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
 
 class AuthTokenController extends Controller {
     /**
@@ -13,15 +15,52 @@ class AuthTokenController extends Controller {
      * @Rest\Post("/authtokens")
      */
     public function postAuthTokensAction(Request $request) {
-//        $user = $this->get('doctrine.orm.entity_manager')
-//            ->getRepository('AuthBundle:AuthToken')
-//            ->findOneByValue($request->headers->get('X-Auth-Token'))->getUser();
-          // Confirmation du compte et création du auth token
-//        $authToken = new AuthToken();
-//        $authToken->setValue($this->generateToken(64));
-//        $authToken->setCreatedAt(new \DateTime('now'));
-//        $authToken->setUser($user);
-//        $user->setAuthToken($authToken);
-//        $em->persist($authToken);
+        $token = $request->request->get('token');
+        if (!$token) {
+            return $this->invalidToken(false);
+        }
+        $em = $this->get('doctrine.orm.entity_manager');
+        $user = $em->getRepository('AuthBundle:User')
+            ->findOneByConfirmationToken($token);
+        if (!$user) {
+            return $this->invalidToken(true);
+        }
+        if ($user->getConfirmed()) {
+            return $this->alreadyConfirmed(true);
+        }
+        $user->setConfirmed(true);
+        $user->setConfirmationToken(null);
+        $authToken = new AuthToken();
+        $authToken->setValue($this->generateToken(64));
+        $authToken->setCreatedAt(new \DateTime('now'));
+        $authToken->setUser($user);
+        $user->setAuthToken($authToken);
+        $em->persist($user);
+        $em->persist($authToken);
+        $em->flush();
+        return $user;
+    }
+
+    private function invalidToken($exist) {
+        if ($exist) {
+            return View::create(['message' => 'Aucun utilisateur avec ce token'], Response::HTTP_BAD_REQUEST);
+        } else {
+            return View::create(['message' => 'Aucun token fourni'], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    private function alreadyConfirmed() {
+        // Normalement ne devrait jamais arriver
+        return View::create(['message' => 'Votre compte a déjà été confirmé'], Response::HTTP_BAD_REQUEST);
+    }
+
+    private function generateToken($length) {
+        $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        $string = '';
+        $max = strlen($characters) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $string .= $characters[mt_rand(0, $max)];
+        }
+        return $string;
     }
 }
