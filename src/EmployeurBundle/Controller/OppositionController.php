7b33de42dad3fa5c0b3322d87d41bd7a137bafeb
poupinette <blacks_ellis@hotmail.com>
2017-03-08 19:15:54 +0100

    [jenifer]:Opposition avec beug

---
+++ src/EmployeurBundle/Controller/OppositionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EmployeurBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+
+
+class OppositionController extends Controller
+{
+    /**
+     * @Rest\View(statusCode=Response::HTTP_OK)
+     * @Rest\Post("/opposition")
+     */
+    public function OppositionAction(Request $request)
+    {
+        $id = $request->query->get("id");
+        $em = $this->get('doctrine.orm.entity_manager');
+        $employe = $em->getRepository("EmployeBundle:Employe")
+            ->find($id);
+        if ($employe == null) {
+            return View::create(['message' => 'Employé introuvable'], Response::HTTP_BAD_REQUEST);
+        }
+        $carte = $employe->getCarte();
+        $carte->setOpposed(true);
+        $em->persist($carte);
+        $em->flush();
+
+        return ["Success" => "L'opposition à bien été effectuée"];
+    }
+
+}
--- src/TransactionBundle/Controller/TransactionController.php
+++ src/TransactionBundle/Controller/TransactionController.php
@@ -10,7 +10,6 @@ use FOS\RestBundle\View\View;
 use TransactionBundle\Entity\Transaction;
 use TransactionBundle\Entity\VerifTransaction;
 use TransactionBundle\Form\VerifTransactionType;
-
 class TransactionController extends Controller {
     /**
      * @Rest\View(statusCode=Response::HTTP_OK)
@@ -29,7 +28,7 @@ class TransactionController extends Controller {
         if ($carte == null) {
             return View::create(['message' => 'Carte introuvable'], Response::HTTP_BAD_REQUEST);
         }
-        $lecteur =$em->getRepository("CommercantBundle:Lecteur")
+        $lecteur = $em->getRepository("CommercantBundle:Lecteur")
             ->findOneByNumero($verifTransaction->getNumeroLecteur());
         if ($lecteur == null) {
             return View::create(['message' => 'Lecteur introuvable'], Response::HTTP_BAD_REQUEST);
@@ -43,7 +42,7 @@ class TransactionController extends Controller {
             return View::create(['message' => 'Votre carte est désactivée, contactez votre employeur'], Response::HTTP_BAD_REQUEST);
         }
         $solde = $carte->getSolde();
-        if ($solde < $verifTransaction->getMontant() ) {
+        if ($solde < $verifTransaction->getMontant()) {
             return View::create(['message' => 'Votre solde est insuffisant'], Response::HTTP_BAD_REQUEST);
         }
         $transaction = new Transaction();
@@ -52,34 +51,50 @@ class TransactionController extends Controller {
         $transaction->setCarte($carte);
         $transaction->setLecteur($lecteur);
         $em->persist($transaction);
-
-        $nouveauSolde= $solde - $verifTransaction->getMontant();
+        $nouveauSolde = $solde - $verifTransaction->getMontant();
         $carte->setSolde($nouveauSolde);
         $em->persist($carte);
         $em->flush();
-
-        $message = \Swift_Message::newInstance();
-        $logoImgUrl = $message->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/logo.png'));
-        $heartImgUrl = $message->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/heart.png'));
-        $name = $lecteur->getCommercant()->getCivilite(). ' ' .$lecteur->getCommercant()->getNom(). ' ' . $lecteur->getCommercant()->getPrenom();
-        $message->setSubject('Nouvelle transaction Aventix')
+        $messageCommercant = \Swift_Message::newInstance();
+        $logoImgUrl = $messageCommercant->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/logo.png'));
+        $heartImgUrl = $messageCommercant->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/heart.png'));
+        $nameCommercant = $lecteur->getCommercant()->getCivilite() . ' ' . $lecteur->getCommercant()->getNom() . ' ' . $lecteur->getCommercant()->getPrenom();
+        $messageCommercant->setSubject('Nouvelle transaction Aventix')
             ->setFrom(array('dwarse.development@gmail.com' => 'Dwarse Team'))
             ->setTo($lecteur->getCommercant()->getUser()->getEmail())
             ->setCharset('utf-8')
             ->setContentType('text/html')
-            ->setBody($this->renderView('@Transaction/Emails/post_transaction.html.twig',
+            ->setBody($this->renderView('@Transaction/Emails/post_transaction_commercant.html.twig',
                 [
                     'logoImgUrl' => $logoImgUrl,
                     'heartImgUrl' => $heartImgUrl,
-                    'name' => $name,
-                    'date' => $transaction->getDate()->format('dd/mm/YYYY'),
+                    'name' => $nameCommercant,
+                    'date' => $transaction->getDate()->format('d/m/Y'),
                     'heure' => $transaction->getDate()->format('H:i'),
                     'montant' => $transaction->getMontant()
                 ]
             ));
-        $this->get('mailer')->send($message);
-
+        $this->get('mailer')->send($messageCommercant);
+        $messageEmploye = \Swift_Message::newInstance();
+        $logoImgUrl = $messageEmploye->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/logo.png'));
+        $heartImgUrl = $messageEmploye->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/heart.png'));
+        $nameEmploye = $carte->getEmploye()->getCivilite() . ' ' . $carte->getEmploye()->getNom() . ' ' . $carte->getEmploye()->getPrenom();
+        $messageEmploye->setSubject('Nouvelle transaction Aventix')
+            ->setFrom(array('dwarse.development@gmail.com' => 'Dwarse Team'))
+            ->setTo($carte->getEmploye()->getUser()->getEmail())
+            ->setCharset('utf-8')
+            ->setContentType('text/html')
+            ->setBody($this->renderView('@Transaction/Emails/post_transaction_employe.html.twig',
+                [
+                    'logoImgUrl' => $logoImgUrl,
+                    'heartImgUrl' => $heartImgUrl,
+                    'name' => $nameEmploye,
+                    'date' => $transaction->getDate()->format('d/m/Y'),
+                    'heure' => $transaction->getDate()->format('H:i'),
+                    'montant' => $transaction->getMontant()
+                ]
+            ));
+        $this->get('mailer')->send($messageEmploye);
         return ["Success" => "La transaction à bien été effectuée"];
     }
-
 }
