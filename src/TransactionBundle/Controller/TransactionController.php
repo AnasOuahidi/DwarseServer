211fccaadbb3a364090f44cb2b963cd317668dad
poupinette <blacks_ellis@hotmail.com>
2017-03-08 18:15:06 +0100

    [jenifer]:mise en place de la transaction et mail

--- src/TransactionBundle/Controller/TransactionController.php
+++ src/TransactionBundle/Controller/TransactionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use TransactionBundle\Entity\Transaction;
 use TransactionBundle\Entity\VerifTransaction;
 use TransactionBundle\Form\VerifTransactionType;
 
@@ -26,15 +27,59 @@ class TransactionController extends Controller {
         $carte = $em->getRepository("EmployeBundle:Carte")
             ->findOneByNumero($verifTransaction->getNumeroCarte());
         if ($carte == null) {
-            return View::create(['message' => 'Vous n\'êtes pas un employeur'], Response::HTTP_BAD_REQUEST);
+            return View::create(['message' => 'Carte introuvable'], Response::HTTP_BAD_REQUEST);
         }
         $lecteur =$em->getRepository("CommercantBundle:Lecteur")
             ->findOneByNumero($verifTransaction->getNumeroLecteur());
         if ($lecteur == null) {
-            return View::create(['message' => 'Vous n\'êtes pas un employeur'], Response::HTTP_BAD_REQUEST);
+            return View::create(['message' => 'Lecteur introuvable'], Response::HTTP_BAD_REQUEST);
         }
+        $pin = $carte->getPassword();
+        if ($pin != $verifTransaction->getPin()) {
+            return View::create(['message' => 'Votre code pin est incorrecte'], Response::HTTP_BAD_REQUEST);
+        }
+        $opposed = $carte->getOpposed();
+        if ($opposed) {
+            return View::create(['message' => 'Votre carte est désactivée, contactez votre employeur'], Response::HTTP_BAD_REQUEST);
+        }
+        $solde = $carte->getSolde();
+        if ($solde < $verifTransaction->getMontant() ) {
+            return View::create(['message' => 'Votre solde est insuffisant'], Response::HTTP_BAD_REQUEST);
+        }
+        $transaction = new Transaction();
+        $transaction->setDate(new \DateTime('now'));
+        $transaction->setMontant($verifTransaction->getMontant());
+        $transaction->setCarte($carte);
+        $transaction->setLecteur($lecteur);
+        $em->persist($transaction);
 
-        return [];
+        $nouveauSolde= $solde - $verifTransaction->getMontant();
+        $carte->setSolde($nouveauSolde);
+        $em->persist($carte);
+        $em->flush();
+
+        $message = \Swift_Message::newInstance();
+        $logoImgUrl = $message->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/logo.png'));
+        $heartImgUrl = $message->embed(\Swift_Image::fromPath('https://s3.amazonaws.com/dwarse/assets/img/heart.png'));
+        $name = $lecteur->getCommercant()->getCivilite(). ' ' .$lecteur->getCommercant()->getNom(). ' ' . $lecteur->getCommercant()->getPrenom();
+        $message->setSubject('Nouvelle transaction Aventix')
+            ->setFrom(array('dwarse.development@gmail.com' => 'Dwarse Team'))
+            ->setTo($lecteur->getCommercant()->getUser()->getEmail())
+            ->setCharset('utf-8')
+            ->setContentType('text/html')
+            ->setBody($this->renderView('@Transaction/Emails/post_transaction.html.twig',
+                [
+                    'logoImgUrl' => $logoImgUrl,
+                    'heartImgUrl' => $heartImgUrl,
+                    'name' => $name,
+                    'date' => $transaction->getDate()->format('dd/mm/YYYY'),
+                    'heure' => $transaction->getDate()->format('H:i'),
+                    'montant' => $transaction->getMontant()
+                ]
+            ));
+        $this->get('mailer')->send($message);
+
+        return ["Success" => "La transaction à bien été effectuée"];
     }
 
 }
